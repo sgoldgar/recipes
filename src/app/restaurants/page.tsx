db00f26f18bd1addefe0874fd9f1c1a3ed8e3864
Sarah Goldgar <sarahgoldgar@Sarahs-MacBook-Air.local>
2023-11-12 20:48:22 -0600

    add sorting, fix some css mobile

--- src/app/restaurants/page.tsx
+++ src/app/restaurants/page.tsx
@@ -5,42 +5,56 @@ import styles from './restaurants.module.css';
 import Select from 'react-select'
 
 
+const sortedRestaurants = restaurants.sort((r1, r2) => (r1.name > r2.name) ? 1 : (r1.name < r2.name) ? -1 : 0);
+
 const categories = ['drinks', 'fancy', 'weekend', 'tiki drinks', 'beer', 'kid friendly', 'dog friendly', 'patio'
 , 'burgers', 'southern', 'sandwiches', 'american', 'seafood', 'pizza', 'quick/casual', 'italian', 'happy hour food'
 , 'japanese', 'frozen drinks', 'mexican', 'bbq', 'asian', 'french', 'mediterranean', 'thai', 'chinese'
 , 'foodtruck', 'indian', 'vietnamese', 'korean', 'brunch', 'bakery', 'coffeeshop', 'desserts', 'pub food', 'latin american'
 , 'cajun', 'jamaican']
 
+const locations = ['North', 'Central', 'South 1st/Lamar', 'East Side', 'Downtown', 'West', 'South', 'Cedar Park']
+
 const categoryOptions = categories.map(category => { return {value: category, label: category}})
+const locationOptions = locations.map(category => { return {value: category, label: category}})
 
 
 export default function Restaurants() {
 
   const [activeCategories, setActiveCategories] = useState([]);
 
-  const [activeRestaurants, setActiveRestaurants] = useState(restaurants)
+  const [activeRestaurants, setActiveRestaurants] = useState(sortedRestaurants);
+  const [activeLocations, setActiveLocations] = useState(locations)
+
 
   const handleUpdateCategories =  async (options: {label: string, value: string}[]) => {
     const categories = options.map(option => option.value);
     // @ts-ignore
     setActiveCategories(categories.length ? categories : [])
-
   }
+
+  const handleUpdateLocations =  async (options: {label: string, value: string}[]) => {
+    const locations = options.map(option => option.value);
+    // @ts-ignore
+    setActiveLocations(locations.length ? locations : [])
+  }
+
+
   const handleUpdateRestaurants =  () => {
-    if(activeCategories.length){
-        const newRests = restaurants.filter((restaurant) => {
-          const restaurantCategories = restaurant.categories;
-          return activeCategories.every(category => restaurantCategories.includes(category))
-      })
-      setActiveRestaurants(newRests)
-    } else {
-      setActiveRestaurants(restaurants)
-    }
+    const filterCategory = sortedRestaurants.filter((restaurant) => {
+        const restaurantCategories = restaurant.categories;
+        return activeCategories.every(category => restaurantCategories.includes(category))
+    })
+    const filterLocation = filterCategory.filter((restaurant) => {
+      return activeLocations.includes(restaurant.location)
+  })
+    setActiveRestaurants(filterLocation)
+    
   }
 
   useEffect(() => {
     handleUpdateRestaurants()
-  }, [activeCategories])
+  }, [activeCategories, activeLocations])
 
 
   return (
@@ -49,17 +63,31 @@ export default function Restaurants() {
         <div className="space-y-4">
           <h1 className="text-center text-5xl">Restaurants</h1>
         </div>
-        <div className={styles.filter}>
-          <label>Category:</label>
-          <Select
-            isMulti
-            name="categories"
-            options={categoryOptions}
-            className="basic-multi-select"
-            classNamePrefix="select"
-            // @ts-ignore
-            onChange={(options) => handleUpdateCategories(options)}
-          />
+        <div className={styles.filterContainer}>
+          <div className={styles.filter}>
+            <label>Category:</label>
+            <Select
+              isMulti
+              name="categories"
+              options={categoryOptions}
+              className="basic-multi-select"
+              classNamePrefix="select"
+              // @ts-ignore
+              onChange={(options) => handleUpdateCategories(options)}
+            />
+          </div>
+          <div className={styles.filter}>
+            <label>Location:</label>
+            <Select
+              isMulti
+              name="locations"
+              options={locationOptions}
+              className="basic-multi-select"
+              classNamePrefix="select"
+              // @ts-ignore
+              onChange={(options) => handleUpdateLocations(options)}
+            />
+          </div>
         </div>
         <div> 
           {activeRestaurants.map((restaurant) => (
